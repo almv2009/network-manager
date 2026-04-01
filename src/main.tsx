b43c7e0d765d1966b73af589d871e2b66ca6be2a
almv2009 <almv2009@gmail.com>
2026-04-01 16:47:18 -0400

    Fix planning flow build errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import EnterpriseApp from "./App";
-import StandaloneApp from "./StandaloneApp";
+import App from "./App";
 import "./index.css";
-import { SessionProvider } from "./session";
-
-const runtimeMode =
-  ((import.meta as ImportMeta & { env?: Record<string, string> }).env?.VITE_APP_RUNTIME || "standalone").trim();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {runtimeMode === "enterprise" ? (
-      <BrowserRouter>
-        <SessionProvider>
-          <EnterpriseApp />
-        </SessionProvider>
-      </BrowserRouter>
-    ) : (
-      <StandaloneApp />
-    )}
+    <App />
   </React.StrictMode>
 );
